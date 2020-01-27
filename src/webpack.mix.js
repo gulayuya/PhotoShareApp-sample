665d8547dfd7153685d83a20715f260a90036bf3
Yuya Sato <gula.yuya14@gmail.com>
2020-01-27 14:18:41 +0900

    set initial settings

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,5 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+    proxy: '127.0.0.1:10080',
+    open: false
+})
+    .js('resources/js/app.js', 'public/js')
+    .version()
